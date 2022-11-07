2c82fd1b8258f76739e5d56c470dc7a4774ca734
mickcaff <mcaffery@outlook.com>
2022-11-07 14:57:00 +1000

    US1 - added landing page component and css global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <LandingPage />
+    </div>
   );
 }
 
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LandingPage.css"
+
+function LandingPage() {
+  return (
+    <main className="landing-main">
+      <h1>LandingPage</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor aliquid quasi, debitis voluptates odio asperiores voluptatem! Est praesentium quos, suscipit, impedit cumque, eius tenetur et sequi saepe aliquid rerum repellendus.</p>
+        <button>Sign Up</button>
+        <button>View Events</button>
+    </main>
+  );
+}
+
+export default LandingPage;
