1e622f7b6913a9616aab0960f03c86de39c30407
Christopher Hullman <christopherhullman@gmail.com>
2022-11-18 10:02:33 +1000

    US2 - Added a new Axios route to our Server API new endpoint to check if a username is valid, before attempting to create the user on Firebase

--- src/firebase/SignUp.js
+++ src/firebase/SignUp.js
@@ -2,6 +2,18 @@ import { firebaseAuth, createUserWithEmailAndPassword } from "./firebaseApp";
 import axios from "axios";
 
 async function SignUp(username, emailAddress, password) {
+  const res = await axios.post(
+    `${process.env.REACT_APP_SERVER_URL}/api/users/check-username-uniqueness`,
+    {
+      username: username,
+    }
+  );
+  if (res.data.usernameExists) {
+    throw new Error(
+      `Error: The username '${username}' is already taken, please try another`
+    );
+  }
+
   // Use Firebase client SDK to try and create a new user on Firebase with email and password
   // and if successful, return the ID token of the user and add their username to their profile
   let token = "";
@@ -13,7 +25,19 @@ async function SignUp(username, emailAddress, password) {
     );
     const user = userCredential.user;
     token = await user.getIdToken();
+  } catch (error) {
+    const errorCode = error.code;
+    console.log(`Error caught creating new user on Firebase: ${error}`);
+    if (errorCode === "auth/email-already-in-use") {
+      throw new Error(
+        "Error: An account with that email address already exists"
+      );
+    } else {
+      throw new Error("Error: Something unexpected occured");
+    }
+  }
 
+  if (token) {
     try {
       const res = await axios.post(
         `${process.env.REACT_APP_SERVER_URL}/api/users/create-current-user`,
@@ -33,17 +57,8 @@ async function SignUp(username, emailAddress, password) {
         `Error in POST to ${process.env.REACT_APP_SERVER_URL}/api/users/create-current-user with error data:`,
         error
       );
-      throw new Error(error);
-    }
-  } catch (error) {
-    const errorCode = error.code;
-    console.log(`Error caught creating new user on Firebase: ${error}`);
-    if (errorCode === "auth/email-already-in-use") {
-      throw new Error(
-        "Error: An account with that email address already exists"
-      );
-    } else {
-      throw new Error(error);
+
+      throw new Error("Error: Something unexpected occured");
     }
   }
 }
